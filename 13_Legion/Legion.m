9c8ea6016cae5eca6194a821b06e6f41031f8256
williamedwardhahn <williamedwardhahn@gmail.com>
2015-09-21 02:51:24 -0400

    Lab Manual Update

---
+++ 13_Legion/Legion.m
@@ -0,0 +1,14 @@
+########################################################
+#------------------------------------------------------#
+#
+# Machine Perception and Cognitive Robotics Laboratory
+#
+#     Center for Complex Systems and Brain Sciences
+#               Florida Atlantic University
+#
+#------------------------------------------------------#
+########################################################
+#------------------------------------------------------#
+#LabManual
+#------------------------------------------------------#
+Legion: Locally Excitatory Globally Inhibitatory Oscillatory Neural Network
---
+++ 18_WhereAreWeGoing/WhereAreWeGoing.py
@@ -0,0 +1,20 @@
+########################################################
+#------------------------------------------------------#
+#
+# Machine Perception and Cognitive Robotics Laboratory
+#
+#     Center for Complex Systems and Brain Sciences
+#               Florida Atlantic University
+#
+#------------------------------------------------------#
+########################################################
+#------------------------------------------------------#
+#LabManual
+#------------------------------------------------------#
+RL_RA_LCA
+
+Reinforcement Learning 
+Recommendation Architecture
+DL-LCA
+Dicionary Learning 
+QLeaning 
