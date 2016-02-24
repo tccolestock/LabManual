07d52a36b70746009a20b4560fcf9e80f980012e
Cutman42 <mpcrfau+turing@gmail.com>
2016-02-24 17:04:08 -0500

    Better explained what was going on and how the Rover class worked. If it doesn't make sense please let me know.

--- Intro/2_Connections/Connections.py
+++ Intro/2_Connections/Connections.py
@@ -15,23 +15,18 @@
 
 #Battery Request
 
-from rover import Rover20
+from rover import Rover
 import time
 
-
+#Rover is a file created to connect this program with the physical device itself.
+# Make sure you see it in the top left of the screen with its other programs like blowfish.
 def main():
-
-
-    rover = Rover20()  # create rover
-    
-    bat=rover.getBatteryPercentage()
-
-	print('Battery level: %d%%' % bat)
-    
+    rover = Rover()  # create rover
+    bat=rover.get_battery_percentage() #Now that the rover is on and using the specific Rover class, you
+    #can now type in the command for battery percentage (which can be found in the Rover class).
+    print('Battery level: %d%%' % bat) #Now tell it to give you the percentage.
     rover.close()  # close rover
+#Now run the program and Voila! You now know how much energy your rover has!
 
 
 main()
-
-
-
